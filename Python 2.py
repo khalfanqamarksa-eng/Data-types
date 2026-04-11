374d75d4286faf76fce216e6b0895c72e1ed5ba9
khalfanqamarksa-eng <khalfanqamar.ksa@gmail.com>
2026-04-11 18:39:20 +0300

    Add files via upload

---
+++ Python 2.py
@@ -0,0 +1,31 @@
+name = "Penguin"
+age = 15
+is_student = True
+weight = 38.5
+
+print  ("Name:", name)
+print ("Data type of name:", type(name))
+
+print ("Age:", age)
+print ("Data type of age:", type(age))
+
+print ("Is student:", is_student)
+print ("Data type of is_student:", type(is_student)) 
+
+print ("Weight:", weight)
+print ("Data type of weight:", type(weight))
+
+# Type casting to convert the datatype of variables
+print ("\n After type casting...")
+age = str(age)
+print (age)
+print ("Data type of age:", type(age))
+weight = int(weight)
+print (weight)
+print ("Data type of weight:", type(weight))
+Is_student = str(is_student)
+print (is_student)
+print ("Data type of is_student:", type(is_student))
+name = int(name)
+print (name)
+print ("Data type of name:", type(name))
---
+++ Python.py
@@ -0,0 +1,17 @@
+print ("Lets check the datatype of different values")
+a = 5
+print ("type of a:", type(a))
+b = 2.5
+print ("type of b:", type(b))
+c = "coding"
+print ("type of c:", type(c))
+d = True
+print ("type of d:", type(d))
+
+name = "Penguin"
+age = 15
+is_student = True
+weight = 38.5
+
+print  ("Name:", name)
+print ("Data type of name:", type(name))
